8daaf3eb7ce373a7f2f69d4562bf4b08eb24cef2
Milan Petkovic <milan96petkovic@gmail.com>
2020-03-23 23:54:32 +0100

    base TDD with REST and AUTH

---
+++ backend/backend/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from django.contrib import admin
+
+# Setup automatic URL routing
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/user/', include('user.urls'))
+]
---
+++ backend/base/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new User"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Jackpot(models.Model):
+    name = models.CharField(max_length=20, null=True, blank=True)
+    value = models.DecimalField(decimal_places=4, max_digits=100)
+
+    def __str__(self):
+        return self.name
